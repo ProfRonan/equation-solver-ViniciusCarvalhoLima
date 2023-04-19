787ebfd4b5d9ccaf6d4322c887c9c9aad9e88d9d
Vinicius Carvalho <viniciuscarvalholima.27@gmail.com>
2023-04-19 11:29:43 -0300

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+Grau = int(input("Grau da Equação:\n"))
+if Grau != int(1) and Grau != int(2):
+        print("Grau inválido")
+elif Grau == 1:
+    print("A equação é do primeiro grau")
+    a = int(input("Digite valor de A"))
+    if a = int(0):
+        print("Valor de a inválido")
+elif Grau == 2:
+    print("A equação é do segundo grau")
+
+
+    INACABADO
